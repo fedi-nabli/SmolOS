e60ce1a5549b8d9ee85a18ac3fb2450ea942db79
fedi-nabli <fedinabli@gmail.com>
2023-05-24 18:29:51 +0100

    Prepared the boot loader to read the kernel

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -6,7 +6,32 @@ _start:
   nop
 
 start:
-  mov si, message
+  ; RESET THE DISK
+  ; DL Register contains current drive as passed to us by bios
+  mov ah, 0x00
+  int 0x13
+
+  ; Segment where kernel will load
+  mov ax, 0x7e0
+  mov es, ax
+
+; LOAD KERNEL INTO MEMORY
+kerenl_load:
+  mov ah, 0x02
+  mov al, 15
+  mov ch, 0
+  mov cl, 2
+  mov dh, 0
+  mov bx, 0x00
+  int 0x13
+  jc .problem
+  
+  mov ax, 0x7e0
+  mov ds, ax
+  ; Jump to the loaded kernel
+  jmp 0x7e0:00
+.problem:
+  mov si, problem_loading_kernel
   call print
   jmp $
 
@@ -23,7 +48,7 @@ print:
   pop ax
   ret
 
-message: db 'Hello World!', 0
+problem_loading_kernel: db 'Issue loading the kernel, please ensure medium is large enough and contains valid kernel', 0
 
 times 510-($ -$$) db 0
 dw 0xAA55
